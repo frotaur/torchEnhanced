e26f668e3cf5e8479cbe35887639a23487bdbf93
Frotaur <frotaur@hotmail.co.uk>
2024-01-20 02:46:44 +0100

    Added 'gridify' function. Like make_grid of pytorch, but faster and more versatile

--- src/torchenhanced/util/visualize.py
+++ src/torchenhanced/util/visualize.py
@@ -107,6 +107,56 @@ def saveTensVideo(tensor,folderpath,name="videotensor",columns=None,fps=30,out_s
         video_tens = tensor.reshape((-1,*tensor.shape[-4:]))
         saveTensVideo(video_tens,folderpath,name,columns,fps,out_size)
 
+
+def gridify(tensor,out_size=800,columns=None):
+    """
+        Makes a grid of images/videos from a batch of images. Accepts (B,*,H,W), 
+
+        Args:
+            tensor : (B,*,H,W) tensor
+            out_size : size of the output grid
+            columns : number of columns of the grid
+        
+        Returns:
+            (*,H',W') tensor, representing the grid of images/videos
+    """
+    B,H,W = tensor.shape[0],tensor.shape[-2],tensor.shape[-1]
+    device = tensor.device
+    if(columns is not None):
+        numCol=columns
+    else :
+        numCol=min(8,B)
+
+
+    black_cols = (-B)%numCol
+    tensor = torch.cat([tensor,torch.zeros(black_cols,*tensor.shape[1:],device=device)],dim=0) # (B',*,H,W)
+    tensor = transf.Pad(3)(tensor) # (B',*,H+3*2,W+3*2)
+
+    B,H,W = tensor.shape[0],tensor.shape[-2],tensor.shape[-1]
+    rest_dim = tensor.shape[1:-2]
+
+    rest_dim_prod = 1
+    for dim in rest_dim:
+        rest_dim_prod*=dim
+    
+    resize_ratio = out_size/(H*numCol)
+
+    indiv_vid_size = int(H*resize_ratio),int(W*resize_ratio)
+    tensor = tensor.reshape((B,rest_dim_prod,H,W))
+    tensor = transf.Resize(indiv_vid_size,antialias=True)(tensor) # (B',rest_dim_prod,H',W')
+    B,H,W = tensor.shape[0],tensor.shape[-2],tensor.shape[-1]
+
+    assert B%numCol==0
+
+    numRows = B//numCol
+
+    tensor = tensor.reshape((numRows,numCol,rest_dim_prod,H,W)) # (numRows,numCol,rest_dim_prod,H',W')
+    tensor = torch.einsum('nmrhw->rnhmw',tensor) # (rest_prod,numRows,H',numCol,W')
+    tensor = tensor.reshape((rest_dim_prod,numRows*H,numCol*W)) # (rest_prod,numRows*H,numCol*W)
+    tensor = tensor.reshape((*rest_dim,numRows*H,numCol*W)) # (*,numRows*H,numCol*W)
+
+    return tensor
+
 @torch.no_grad()
 def saveTensImage(tensor, folderpath,name="imagetensor",columns=None):
     '''
