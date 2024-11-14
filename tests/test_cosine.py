4d30494af15a9879946182f0e92ce73fb1b31413
frotaur <frotaur@hotmail.co.uk>
2024-11-14 02:03:14 +0100

    Ruff-ed, added load/saving utils. Missing easy resume batch and ready for v4

--- tests/test_cosine.py
+++ tests/test_cosine.py
@@ -1,14 +1,16 @@
-import torch,sys,pathlib
+import torch, sys, pathlib
+
 sys.path.append(pathlib.Path(__file__).parent.parent.as_posix())
 from src.torchenhanced.ml.optim import CosineWarmup
 import matplotlib.pyplot as plt
 
+
 def test_cosine():
-    pass #(for manual thingy only)
+    pass  # (for manual thingy only)
     # opti = torch.optim.SGD([torch.zeros(1)],lr=0.1)
     # sched = CosineWarmup(optimizer=opti, warmup_steps=10, T_0=100,
     #                      lr_shrink=0.8,lr_init=1e-3,T_mult=1,eta_min=1e-3)
-    
+
     # lrs = []
     # epochs = []
     # for ep in range(600):
@@ -21,5 +23,6 @@ def test_cosine():
     # # Plot lrs against epochs
     # plt.plot(epochs,lrs)
     # plt.show()
+
 
 # test_cosine()
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,34 +1,38 @@
-import torch,sys,pathlib
+import torch, sys, pathlib
+
 sys.path.append(pathlib.Path(__file__).parent.parent.as_posix())
 
 from src.torchenhanced.util import *
 from src.torchenhanced.util.misc import coord_to_flat
 
+
 def test_coord_to_flat_batch():
-    unfolded = torch.randint(0,100,(3,4,5,6)) #4-dim coord tensor
+    unfolded = torch.randint(0, 100, (3, 4, 5, 6))  # 4-dim coord tensor
     folded = unfolded.reshape((-1))
 
     dimensions = unfolded.shape
-    testcoord = torch.tensor([[1,2,3,4],[0,2,3,2]]) # Set of coordinate points
+    testcoord = torch.tensor([[1, 2, 3, 4], [0, 2, 3, 2]])  # Set of coordinate points
 
-    flatcoord = coord_to_flat(testcoord,dimensions)
+    flatcoord = coord_to_flat(testcoord, dimensions)
     assert flatcoord.shape == (2,)
     res = folded[flatcoord]
     for i in range(testcoord.shape[0]):
-        assert (res[i]-unfolded[testcoord[i][0]][testcoord[i][1]][testcoord[i][2]][testcoord[i][3]]).all()==0
+        assert (res[i] - unfolded[testcoord[i][0]][testcoord[i][1]][testcoord[i][2]][testcoord[i][3]]).all() == 0
+
 
 def test_coord_to_flat_unbatch():
-    unfolded = torch.randint(0,100,(3,4,5,6)) #4-dim coord tensor
+    unfolded = torch.randint(0, 100, (3, 4, 5, 6))  # 4-dim coord tensor
     folded = unfolded.reshape((-1))
 
     dimensions = unfolded.shape
-    testcoord = torch.tensor([1,2,3,4]) # Set of coordinate points
+    testcoord = torch.tensor([1, 2, 3, 4])  # Set of coordinate points
 
-    flatcoord = coord_to_flat(testcoord,dimensions)
-    assert flatcoord.shape==(), f"Invalid shape : {flatcoord.shape}"
+    flatcoord = coord_to_flat(testcoord, dimensions)
+    assert flatcoord.shape == (), f"Invalid shape : {flatcoord.shape}"
     res = folded[flatcoord]
-    
-    assert (res-unfolded[testcoord[0]][testcoord[1]][testcoord[2]][testcoord[3]]).all()==0
+
+    assert (res - unfolded[testcoord[0]][testcoord[1]][testcoord[2]][testcoord[3]]).all() == 0
+
 
 def test_color_comp_incomplete():
     from PIL import Image
@@ -47,16 +51,13 @@ def test_color_comp_incomplete():
     # imgex=decode_from_nbit(imgex,ntar)
     # print("Decoded again : ",imgex.squeeze())
 
-
     # imgex= Image.open("Riffelsee.jpg")
     # imgex=t.ToTensor()(imgex)[None]
     # showTens(imgex)
     # imgex=rgb_to_yuv(imgex)
-    
-    
+
     # imgn = convert_to_nbit(imgex,ntar)
     # imgn = decode_from_nbit(imgn,ntar)
-
 
     # print("shape : ",imgex.shape)
 
